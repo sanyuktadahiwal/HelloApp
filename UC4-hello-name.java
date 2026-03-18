b610f9a568a99f22a22605fbbdffdc8298bfd5ec
sanyuktadahiwal <sanyuktadahiwal7@gmail.com>
2026-03-18 13:22:23 +0530

    UC4: Display Hello with Multiple Command-Line Arguments

---
+++ UC4-hello-name.java
@@ -0,0 +1,13 @@
+public class UC4-hello-name {
+    public static void main(String[] args) {
+        StringBuilder nameBuilder = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            nameBuilder.append(args[i]);
+            if (i < args.length - 1) {
+                nameBuilder.append(" ");
+            }
+        }
+        name = nameBuilder.toString();
+        System.out.println("Hello, " + name + "!");
+    }
+}
